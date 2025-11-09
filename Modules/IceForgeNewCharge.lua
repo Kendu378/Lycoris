b457f61934208e8aa38661d3aac34dbe8a027420
Blastbrean <73199280+Blastbrean@users.noreply.github.com>
2025-11-08 20:50:18 -0800

    fix(Defender): Remove unused notify parameter and ensure action execution notification is always sent
    fix(HitboxOptions): Initialize cache to zero when hitbox is not defined
    
    refactor(IceForgeNewCharge): Simplify action handling and improve readability of projectile tracking logic
    
    chore: Add new timing patches for animation modifications

--- Modules/IceForgeNewCharge.lua
+++ Modules/IceForgeNewCharge.lua
@@ -15,49 +15,48 @@ local Defense = getfenv().Defense
 ---@param self AnimatorDefender
 ---@param timing AnimationTiming
 return function(self, timing)
-	task.spawn(function()
-		local thrown = workspace:FindFirstChild("Thrown")
-		if not thrown then
-			return
-		end
+	local thrown = workspace:FindFirstChild("Thrown")
+	if not thrown then
+		return
+	end
 
-		local tracker = ProjectileTracker.new(function(candidate)
-			return candidate.Name == "IceShuriken"
-		end)
-		print("foo")
-		task.wait(0.20 - self.rtt())
-		print("foo2")
-		if self:distance(self.entity) <= 20 then
-			local action = Action.new()
-			action._type = "Start Block"
-			action._when = 0
-			action.name = "Ice Forge Close Timing"
-			action.fhb = true
-			action.ihbc = false
-			self:action(timing, action)
-
-			local actionTwo = Action.new()
-			actionTwo._type = "End Block"
-			actionTwo._when = 800
-			actionTwo.fhb = true
-			actionTwo.ihbc = false
-			return self:action(timing, actionTwo)
-		end
-
-		local action = Action.new()
-		action._when = 0
-		action._type = "Parry"
-		action.name = "Ice Forge Part"
-
-		local pt = PartTiming.new()
-		pt.uhc = true
-		pt.duih = true
-		pt.fhb = false
-		pt.name = "IceForgeProjectile"
-		pt.hitbox = Vector3.new(35, 35, 35)
-		pt.actions:push(action)
-		pt.cbm = true
-
-		Defense.cdpo(tracker:wait(), pt)
+	local tracker = ProjectileTracker.new(function(candidate)
+		return candidate.Name == "IceShuriken"
 	end)
+
+	task.wait(0.20 - self.rtt())
+
+	if self:distance(self.entity) <= 20 then
+		local action = Action.new()
+		action._type = "Start Block"
+		action._when = 0
+		action.name = "Ice Forge Close Timing"
+		action.fhb = true
+		action.hitbox = Vector3.new(35, 35, 35)
+		self:action(timing, action)
+
+		local actionTwo = Action.new()
+		actionTwo.name = "Ice Forge Close End Timing"
+		actionTwo._type = "End Block"
+		actionTwo._when = 1400
+		actionTwo.fhb = true
+		actionTwo.ihbc = true
+		return self:action(timing, actionTwo)
+	end
+
+	local action = Action.new()
+	action._when = 0
+	action._type = "Parry"
+	action.name = "Ice Forge Part"
+
+	local pt = PartTiming.new()
+	pt.uhc = true
+	pt.duih = true
+	pt.fhb = false
+	pt.name = "IceForgeProjectile"
+	pt.hitbox = Vector3.new(35, 35, 35)
+	pt.actions:push(action)
+	pt.cbm = true
+
+	Defense.cdpo(tracker:wait(), pt)
 end
