54f228cc41fc55b02cb3dc017897b16111f8a853
John Doe <johndoe@example.com>
2025-11-11 12:40:33 -0800

    feat(animation): Update multiple animation patches with various changes
    - Modified animation properties for several assets, including hitbox adjustments, status changes, and parameter updates.
    - Adjusted parameters such as 'umoa', 'duih', 'pfh', 'smod', and 'hso' across multiple patches to enhance animation behavior.
    - Implemented changes to hitbox dimensions and action definitions for improved gameplay mechanics.
    - Consolidated updates for assets with IDs: 108775350619127, 8310877920, 11493924588, 5828315760, 12138869158, and others.

--- Modules/IceForgeNewCharge.lua
+++ Modules/IceForgeNewCharge.lua
@@ -15,39 +15,54 @@ local Defense = getfenv().Defense
 ---@param self AnimatorDefender
 ---@param timing AnimationTiming
 return function(self, timing)
-	local tracker = ProjectileTracker.new(function(candidate)
-		return candidate.Name == "IceShuriken"
-	end)
+	task.spawn(function()
+		local thrown = workspace:FindFirstChild("Thrown")
+		if not thrown then
+			return
+		end
 
-	task.wait(0.20 - self.rtt())
-	if self:distance(self.entity) <= 20 then
+		local tracker = ProjectileTracker.new(function(candidate)
+			return candidate.Name == "IceShuriken"
+		end)
+
+		task.wait(0.9 - self.rtt())
+
+		if self:distance(self.entity) <= 20 then
+			local action = Action.new()
+			action._type = "Start Block"
+			action._when = 400
+			action.name = "Ice Forge Close Timing"
+			action.fhb = true
+			action.ihbc = false
+			self:action(timing, action)
+
+			local actionTwo = Action.new()
+			actionTwo._type = "End Block"
+			actionTwo._when = 700
+			actionTwo.fhb = true
+			actionTwo.ihbc = false
+			self:action(timing, actionTwo)
+		end
+
 		local action = Action.new()
-		action._type = "Start Block"
 		action._when = 0
-		action.hitbox = Vector3.new(50, 50, 50)
-		action.name = "Ice Forge Close Timing"
-		self:action(timing, action)
+		action._type = "Start Block"
+		action.name = "Ice Forge Part"
 
 		local actionTwo = Action.new()
+		actionTwo._when = 300
 		actionTwo._type = "End Block"
-		actionTwo._when = 800
-		actionTwo.hitbox = Vector3.new(80, 80, 80)
-		self:action(timing, actionTwo)
-	end
+		actionTwo.name = "Ice Forge Part"
 
-	local action = Action.new()
-	action._when = 0
-	action._type = "Parry"
-	action.name = "Ice Forge Part"
+		local pt = PartTiming.new()
+		pt.uhc = true
+		pt.duih = true
+		pt.fhb = false
+		pt.name = "IceForgeProjectile"
+		pt.hitbox = Vector3.new(35, 35, 35)
+		pt.actions:push(action)
+		pt.cbm = true
 
-	local pt = PartTiming.new()
-	pt.uhc = true
-	pt.duih = true
-	pt.fhb = false
-	pt.name = "IceForgeProjectile"
-	pt.hitbox = Vector3.new(35, 35, 35)
-	pt.actions:push(action)
-	pt.cbm = true
-
-	Defense.cdpo(tracker:wait(), pt)
+		Defense.cdpo(tracker:wait(), pt)
+	end)
 end
