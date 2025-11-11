c4e7ef63f1abf497826f708502314e1e4e4549e0
juanitoelgordito <221791452+juanitoelgordito@users.noreply.github.com>
2025-11-11 12:40:33 -0800

    feat(animation): Update multiple animation patches with various changes
    - Modified animation properties for several assets, including hitbox adjustments, status changes, and parameter updates.
    - Adjusted parameters such as 'umoa', 'duih', 'pfh', 'smod', and 'hso' across multiple patches to enhance animation behavior.
    - Implemented changes to hitbox dimensions and action definitions for improved gameplay mechanics.
    - Consolidated updates for assets with IDs: 108775350619127, 8310877920, 11493924588, 5828315760, 12138869158, and others.

--- Modules/TableFlip.lua
+++ Modules/TableFlip.lua
@@ -1,55 +1,20 @@
----@type PartTiming
-local PartTiming = getfenv().PartTiming
-
 ---@type Action
 local Action = getfenv().Action
-
----@type ProjectileTracker
----@diagnostic disable-next-line: unused-local
-local ProjectileTracker = getfenv().ProjectileTracker
-
----@module Features.Combat.Defense
-local Defense = getfenv().Defense
 
 ---Module function.
 ---@param self AnimatorDefender
 ---@param timing AnimationTiming
 return function(self, timing)
-	local thrown = workspace:FindFirstChild("Thrown")
-	if not thrown then
-		return
-	end
-
-	local tracker = ProjectileTracker.new(function(candidate)
-		return candidate.Name == "Secondary"
-	end)
-
-	task.wait(0.5 - self.rtt())
-
-	if self:distance(self.entity) <= 20 then
-		local action = Action.new()
-		action._type = "Parry"
-		action._when = 0
-		action.name = "Table Flip Close Timing"
-		action.hitbox = Vector3.new(20, 20, 20)
-		action.fhb = true
-		action.ihbc = false
-		return self:action(timing, action)
-	end
+	local distance = self:distance(self.entity)
 
 	local action = Action.new()
-	action._when = 0
+	action._when = math.min(400 + distance * 10.5)
+	if self:distance(self.entity) > 51 then
+		action._when = 1025
+	end
 	action._type = "Parry"
-	action.name = "Table Flip Part"
+	action.hitbox = Vector3.new(20, 20, 70)
+	action.name = string.format("(%.2f) Dynamic Table Flip Timing", distance)
 
-	local pt = PartTiming.new()
-	pt.uhc = true
-	pt.duih = true
-	pt.fhb = true
-	pt.name = "TableFlipProjectile"
-	pt.hitbox = Vector3.new(20, 20, 20)
-	pt.actions:push(action)
-	pt.cbm = true
-
-	Defense.cdpo(tracker:wait(), pt)
+	return self:action(timing, action)
 end
